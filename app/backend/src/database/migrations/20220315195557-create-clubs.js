2e50d3573e3c91c88ffe4b1903b33ca0f37f7d02
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-03-15 18:14:54 -0300

    Impl: migration e mode de clubs

---
+++ app/backend/src/database/migrations/20220315195557-create-clubs.js
@@ -0,0 +1,21 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('clubs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      clubName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        field: 'club_name',
+      },
+    });
+  },
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('clubs');
+  }
+};
---
+++ app/backend/src/database/models/Clubs.ts
@@ -0,0 +1,42 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+// import OtherModel from './OtherModel';
+
+class Clubs extends Model {
+  // public <campo>!: <tipo>;
+  declare id: number;
+
+  declare clubName: string;
+}
+
+Clubs.init({
+  // ... Campos
+  id: {
+    type: DataTypes.INTEGER.UNSIGNED,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  clubName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  // ... Outras configs
+  underscored: true,
+  sequelize: db,
+  // modelName: 'example',
+  timestamps: false,
+});
+
+/**
+  * `Workaround` para aplicar as associations em TS:
+  * Associations 1:N devem ficar em uma das instâncias de modelo
+  * */
+
+// OtherModel.belongsTo(Example, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
+// OtherModel.belongsTo(Example, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
+
+// Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
+// Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
+
+export default Clubs;
