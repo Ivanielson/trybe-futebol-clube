8a8d373e1dcb662431952f76bc6d165117c27af5
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-03-18 18:58:03 -0300

    Impl: getById de clubs na camada services

--- app/backend/src/services/clubServices.ts
+++ app/backend/src/services/clubServices.ts
@@ -4,8 +4,17 @@ import Clubs from '../database/models/Clubs';
 export default class ClubesService {
   static async getAll() {
     try {
-      const allClubs = await Clubs.findAll({ raw: true });
-      return allClubs as unknown as IClub[];
+      const clubs = await Clubs.findAll({ raw: true });
+      return clubs as unknown as IClub[];
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  static async getById(id: number) {
+    try {
+      const club = await Clubs.findByPk(id, { raw: true });
+      return club as unknown as IClub;
     } catch (error) {
       console.error(error);
     }
