0570d310095883af69118d47cf82dac33fad839c
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-03-18 13:35:54 -0300

    Refatora autenticação de usuárioa na camada controllers

--- app/backend/src/controllers/loginController.ts
+++ app/backend/src/controllers/loginController.ts
@@ -4,24 +4,21 @@ import { Request, Response } from 'express';
 import ILogin from '../interfaces/ILogin';
 import LoginService from '../services/loginService';
 import StatusCode from '../enums/StatusCode';
-import toUserLogin from '../functions/toUserLogin';
+import { toUserLogin } from '../functions/toUserLogin';
 
 const SECRET = fs.readFileSync('jwt.evaluation.key', 'utf-8');
-const MESSAGE_ERROR_INCORRECT = 'Incorrect email or password';
+// const MESSAGE_ERROR_INCORRECT = 'Incorrect email or password';
 
 export default class LoginController {
   static async login(req: Request, res: Response) {
     try {
       const user = req.body as ILogin;
-      const userValid = await LoginService.authentication(user);
-      if (!userValid) {
-        return res.status(StatusCode.UNAUTHORIZED).json({
-          message: MESSAGE_ERROR_INCORRECT,
-        });
+      const userValid = await LoginService.authentication(user.email);
+      if (userValid) {
+        const token = Jwt.sign({ data: userValid }, SECRET);
+        const result = toUserLogin(userValid, token);
+        res.status(StatusCode.OK).json(result);
       }
-      const token = Jwt.sign({ data: userValid }, SECRET);
-      const result = toUserLogin(userValid, token);
-      res.status(StatusCode.OK).json(result);
     } catch (error) {
       console.error(error);
     }
