dd2d625988b7ed0b325df401a67961208a2fd8c8
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-03-18 11:51:40 -0300

    Refatora loginRouter

--- app/backend/src/routes/loginRouter.ts
+++ app/backend/src/routes/loginRouter.ts
@@ -1,15 +1,16 @@
-import { Request, Response, Router } from 'express';
-import ILogin from '../interfaces/ILogin';
+import { Router } from 'express';
 import LoginController from '../controllers/loginController';
-import validLogin from '../middlewares/loginValidate';
+import * as validate from '../middlewares/loginValidate';
 
 const loginRoute = Router();
-const loginController = new LoginController();
 
-loginRoute.post('/', validLogin, async (req: Request, res: Response) => {
-  const user = req.body as ILogin;
-  const login = await loginController.authentication(user);
-  res.status(200).json(login);
-});
+loginRoute.post(
+  '/',
+  validate.emailExists,
+  validate.passwordExist,
+  validate.emailValid,
+  validate.passwordValid,
+  LoginController.login,
+);
 
 export default loginRoute;
