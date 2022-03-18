b2b5394367b1d658785d837602053a072b869673
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-03-18 18:59:12 -0300

    Impl: Rota 'GET /clubs/:id'

--- app/backend/src/routes/clubRouter.ts
+++ app/backend/src/routes/clubRouter.ts
@@ -3,9 +3,7 @@ import ClubController from '../controllers/clubController';
 
 const clubsRouter = Router();
 
-clubsRouter.get(
-  '/',
-  ClubController.getAll,
-);
+clubsRouter.get('/', ClubController.getAll);
+clubsRouter.get('/:id', ClubController.getById);
 
 export default clubsRouter;
