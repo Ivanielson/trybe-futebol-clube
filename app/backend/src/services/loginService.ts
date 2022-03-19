9094526af601c28770b7671151746b6a80d0e1ec
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-03-19 11:20:59 -0300

    Impl: validateToken na camada services

--- app/backend/src/services/loginService.ts
+++ app/backend/src/services/loginService.ts
@@ -1,6 +1,18 @@
+import fs = require('fs');
+import Jwt = require('jsonwebtoken');
 import IUser from '../interfaces/IUser';
 import Users from '../database/models/Users';
-// import ILogin from '../interfaces/ILogin';
+
+const SECRET = fs.readFileSync('jwt.evaluation.key', 'utf-8');
+type VerifyJWT = {
+  data: {
+    id: number;
+    username: string;
+    role: string;
+    email: string;
+    password: string;
+  }
+};
 
 export default class LoginService {
   static async authentication(email: string): Promise<IUser | undefined> {
@@ -14,5 +26,10 @@ export default class LoginService {
     } catch (error) {
       console.error(error);
     }
+  }
+
+  static validateToken(token: string): string {
+    const { data } = Jwt.verify(token, SECRET) as VerifyJWT;
+    return data.role;
   }
 }
