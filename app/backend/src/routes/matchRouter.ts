bb5be199221f96a5d25c51448b330a6f193349ae
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-03-21 20:56:34 -0300

    Impl: Rota 'POST /matchs'

--- app/backend/src/routes/matchRouter.ts
+++ app/backend/src/routes/matchRouter.ts
@@ -1,8 +1,17 @@
 import { Router } from 'express';
+import { areDifferentTeams, teamExist } from '../middlewares/matchValidate';
 import MatchContoller from '../controllers/matchController';
+import verifyJWT from '../middlewares/validateJWT';
 
 const matchRouter = Router();
 
 matchRouter.get('/', MatchContoller.getAll);
+matchRouter.post(
+  '/',
+  areDifferentTeams,
+  teamExist,
+  verifyJWT,
+  MatchContoller.create,
+);
 
 export default matchRouter;
