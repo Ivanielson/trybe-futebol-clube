3efbece951882a952e755bdc23bd195b149a0fad
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-03-21 20:58:52 -0300

    Refatora login na camada controllers

--- app/backend/src/controllers/loginController.ts
+++ app/backend/src/controllers/loginController.ts
@@ -1,23 +1,23 @@
-import Jwt = require('jsonwebtoken');
-import fs = require('fs');
 import { Request, Response } from 'express';
-import ILogin from '../interfaces/ILogin';
+import { ILogin } from '../interfaces/ILogin';
 import LoginService from '../services/loginService';
 import StatusCode from '../enums/StatusCode';
 import { toUserLogin } from '../functions/helpers';
-
-const SECRET = fs.readFileSync('jwt.evaluation.key', 'utf-8');
+import generatorJwt from '../functions/generatorJWT';
 
 export default class LoginController {
   static async login(req: Request, res: Response) {
     try {
       const user = req.body as ILogin;
       const userValid = await LoginService.authentication(user.email);
-      if (userValid) {
-        const token = Jwt.sign({ data: userValid }, SECRET);
-        const result = toUserLogin(userValid, token);
-        res.status(StatusCode.OK).json(result);
+      if (!userValid) {
+        return res.status(StatusCode.UNAUTHORIZED).json({
+          message: 'Incorrect email or password',
+        });
       }
+      const token = generatorJwt(userValid);
+      const result = toUserLogin(userValid, token);
+      res.status(StatusCode.OK).json(result);
     } catch (error) {
       console.error(error);
     }
