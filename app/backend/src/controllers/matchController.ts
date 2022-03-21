930a3613d5b6450f5f027f6feed159c20d6ef293
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-03-21 20:49:32 -0300

    Impl: create de match na camada controllers

--- app/backend/src/controllers/matchController.ts
+++ app/backend/src/controllers/matchController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { IMatchNew } from '../interfaces/IMatch';
 import StatusCode from '../enums/StatusCode';
 import MatchService from '../services/matchService';
 
@@ -7,6 +8,20 @@ export default class MatchContoller {
     try {
       const matchs = await MatchService.getAll();
       return res.status(StatusCode.OK).json(matchs);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  static async create(req: Request, res: Response) {
+    try {
+      const {
+        homeTeam, awayTeam, homeTeamGoals, awayTeamGoals, inProgress,
+      } = req.body as IMatchNew;
+      const newMatch = await MatchService.create({
+        homeTeam, awayTeam, homeTeamGoals, awayTeamGoals, inProgress,
+      });
+      return res.status(StatusCode.OK).json(newMatch);
     } catch (error) {
       console.error(error);
     }
