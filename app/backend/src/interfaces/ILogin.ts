f17e5196132b03d40427f60a7e0b08783a54f272
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-03-21 20:57:50 -0300

    Impl: interface ILoginValid

--- app/backend/src/interfaces/ILogin.ts
+++ app/backend/src/interfaces/ILogin.ts
@@ -1,4 +1,19 @@
-export default interface ILogin {
+interface ILogin {
   email: string;
   password: string;
 }
+
+interface ILoginValid {
+  user: {
+    id: number,
+    username: string,
+    role: string,
+    email: string,
+  },
+  token: string,
+}
+
+export {
+  ILogin,
+  ILoginValid,
+};
