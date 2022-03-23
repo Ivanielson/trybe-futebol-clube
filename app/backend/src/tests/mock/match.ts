9eff6f4e8a1aa43fe963f279319565aee6f19acb
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-03-23 20:56:23 -0300

    Refatora mock de Matchs

---
+++ app/backend/src/tests/mock/match.ts
@@ -0,0 +1,57 @@
+const matchs = [
+  {
+    id: 1,
+    homeTeam: 16,
+    homeTeamGoals: 1,
+    awayTeam: 8,
+    awayTeamGoals: 1,
+    inProgress: false,
+    homeClub: {
+      clubName: "São Paulo"
+    },
+    awayClub: {
+      clubName: "Grêmio"
+    }
+  },
+  {
+    id: 2,
+    homeTeam: 9,
+    homeTeamGoals: 1,
+    awayTeam: 14,
+    awayTeamGoals: 1,
+    inProgress: false,
+    homeClub: {
+      clubName: "Internacional"
+    },
+    awayClub: {
+      clubName: "Santos"
+    }
+  },
+  {
+    id: 3,
+    homeTeam: 4,
+    homeTeamGoals: 3,
+    awayTeam: 11,
+    awayTeamGoals: 0,
+    inProgress: false,
+    homeClub: {
+      clubName: "Corinthians"
+    },
+    awayClub: {
+      clubName: "Napoli-SC"
+    }
+  }
+];
+
+const matchCreate = {
+  homeTeam: 16,
+  awayTeam: 8,
+  homeTeamGoals: 2,
+  awayTeamGoals: 2,
+  inProgress: true
+};
+
+export {
+  matchs,
+  matchCreate,
+};
