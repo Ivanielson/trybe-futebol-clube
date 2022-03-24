8ef50db1041ddfbe3ff7948a110cca3df71d5a6a
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-03-23 22:53:03 -0300

    Impl: test de login

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -5,68 +5,99 @@ import * as Sinon from "sinon";
 import { Response } from 'superagent';
 
 import { app } from '../app';
-import LoginService from "../services/loginService";
+import { ILoginValid } from '../interfaces/ILogin';
 import IUser from "../interfaces/IUser";
-import loginUser = require('./mock/user.json');
+import LoginService from "../services/loginService";
+import { userValid } from './mock/login';
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Rota "POST /login"', () => {
-  let chaiHttpResponse: Response;
-  const payload = {
-    email: 'admin@admin.com',
-    password: 'secret_admin'
-  };
+describe('Rota login', () => {
+  describe('Rota "POST /login"', () => {
+    let chaiHttpResponse: Response;
+    const payLoad = {
+      email: "admin@admin.com",
+      password: "secret_admin",
+    };
+  
+    before(async () => {
+      Sinon
+        .stub(LoginService, 'authentication')
+        .resolves(userValid as ILoginValid);
+    });
+  
+    after(() => {
+      (LoginService.authentication as Sinon.SinonStub).restore();
+    });
+  
+    it(`Deveria retonar um objeto com as propriedades:
+      user, user.id, user.userName, user.role, user.email e token`, async () => {
+      chaiHttpResponse = await chai
+        .request(app)
+        .post('/login')
+        .send(payLoad);
 
-  before(async () => {
-    Sinon
-      .stub(LoginService, 'authentication')
-      .resolves(loginUser[0] as IUser);
+      expect(chaiHttpResponse.body).to.be.have.a.property('user');
+      expect(chaiHttpResponse.body).to.be.have.a.property('token');
+      expect(chaiHttpResponse.body.user).to.be.have.a.property('username');
+      expect(chaiHttpResponse.body.user).to.be.have.a.property('role');
+      expect(chaiHttpResponse.body.user).to.be.have.a.property('email');
+    });
+
+    it('Deveria retornar o status da requisição: 201', async () => {
+      chaiHttpResponse = await chai
+        .request(app)
+        .post('/login')
+        .send(payLoad);
+
+      expect(chaiHttpResponse.status).to.be.equal(200);
+    });
   });
+  
+  describe('Rota "GET /login/validate"', () => {
+    let chaiHttpResponse: Response;
+    let chaiHttpLogin: Response;
+    const role = "admin";
+    const user = {
+      email: "admin@admin.com",
+      password: "secret_admin",
+    }
 
-  after(() => {
-    (LoginService.authentication as Sinon.SinonStub).restore();
-  });
+    before(async () => {
+      Sinon
+        .stub(LoginService, 'validateToken')
+        .resolves(role);
 
-  it('Se usuário válido retorna um object', async () => {
-    chaiHttpResponse = await chai
-         .request(app)
-         .post('/login')
-         .send(payload);
+      chaiHttpLogin = await chai
+        .request(app)
+        .post('/login')
+        .send(user);
+    });
+  
+    after(() => {
+      (LoginService.validateToken as Sinon.SinonStub).restore();
+    });
+  
+    it('Deveria retornar o role do user ao receber um token válido', async () => {
+      const token = chaiHttpLogin.body.token;
+      chaiHttpResponse = await chai
+        .request(app)
+        .get('/login/validate')
+        .set({ Authorization: token });
 
-    expect(chaiHttpResponse.body).to.be.have.a('object');
-    expect(chaiHttpResponse.body).to.be.have.a.property('id');
-    expect(chaiHttpResponse.body).to.be.have.a.property('username');
-    expect(chaiHttpResponse.body).to.be.have.a.property('role');
-    expect(chaiHttpResponse.body).to.be.have.a.property('email');
-    expect(chaiHttpResponse.body).to.be.have.a.property('password');
-  });
-});
+      expect(chaiHttpResponse.body).to.be.equal(role);
+    });
 
-describe('Rota "GET /login/validate"', () => {
-  let chaiHttpResponse: Response;
-  const role = "admin";
-  const payload = {
-    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImlkIjoxLCJ1c2VybmFtZSI6IkFkbWluIiwicm9sZSI6ImFkbWluIiwiZW1haWwiOiJhZG1pbkBhZG1pbi5jb20iLCJwYXNzd29yZCI6IiQyYSQwOCR4aS5IeGsxY3pBTzBuWlIuLkIzOTN1MTBhRUQwUlExTjNQQUVYUTdIeHRMaktQRVpCdS5QVyJ9LCJpYXQiOjE2NDc5NTYxODksImV4cCI6MTY0ODA0MjU4OX0.TAtm1AZuXfkRysg0O2cjimR_pKYwUa9_n1FvWlbAyNg',
-  };
+    it('Deveria retornar o status da requisição: 200', async () => {
+      const token = chaiHttpLogin.body.token;
+      chaiHttpResponse = await chai
+        .request(app)
+        .get('/login/validate')
+        .set({ Authorization: token });
 
-  before(async () => {
-    Sinon
-      .stub(LoginService, 'validateToken')
-      .resolves(role);
-  });
-
-  after(() => {
-    (LoginService.validateToken as Sinon.SinonStub).restore();
-  });
-
-  it('Recebe um token válido', async () => {
-    chaiHttpResponse = await chai
-         .request(app)
-         .get('/login/validate')
-         .send(payload);
-    expect(chaiHttpResponse.body).to.be.equal(role);
+      expect(chaiHttpResponse.status).to.be.equal(200);
+    });
   });
 });
