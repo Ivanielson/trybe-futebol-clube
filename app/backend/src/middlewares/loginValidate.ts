a0d72c7a30240de47352267587e875e81b1509f3
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-03-23 23:09:29 -0300

    Refatora funçãoes de validações de login

--- app/backend/src/middlewares/loginValidate.ts
+++ app/backend/src/middlewares/loginValidate.ts
@@ -7,47 +7,11 @@ import { checkPassword } from '../functions/helpers';
 const MESSAGE_ERROR = 'All fields must be filled';
 const MESSAGE_ERROR_INCORRECT = 'Incorrect email or password';
 
-const emailExists = (req: Request, res: Response, next: NextFunction) => {
-  const { body } = req;
-  const bodyKeys = Object.keys(body);
-  if (!bodyKeys.includes('email')) {
+const validFields = (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
     return res.status(StatusCode.UNAUTHORIZED).json({
       message: MESSAGE_ERROR,
-    });
-  }
-
-  next();
-};
-
-const passwordExist = (req: Request, res: Response, next: NextFunction) => {
-  const { body } = req;
-  const bodyKeys = Object.keys(body);
-  if (!bodyKeys.includes('password')) {
-    return res.status(StatusCode.UNAUTHORIZED).json({
-      message: MESSAGE_ERROR,
-    });
-  }
-
-  next();
-};
-
-const passwordValid = (req: Request, res: Response, next: NextFunction) => {
-  const { password } = req.body;
-  if (password.length <= 6 || typeof password !== 'string') {
-    return res.status(StatusCode.UNAUTHORIZED).json({
-      message: MESSAGE_ERROR,
-    });
-  }
-
-  next();
-};
-const emailValid = (req: Request, res: Response, next: NextFunction) => {
-  const { email } = req.body;
-  const reg = /\S+@\S+\.\S+/;
-  const validate = reg.test(email);
-  if (!validate) {
-    return res.status(StatusCode.UNAUTHORIZED).json({
-      message: MESSAGE_ERROR_INCORRECT,
     });
   }
 
@@ -73,9 +37,6 @@ const validUser = async (req: Request, res: Response, next: NextFunction) => {
 };
 
 export {
-  emailExists,
-  passwordExist,
-  emailValid,
-  passwordValid,
   validUser,
+  validFields,
 };
