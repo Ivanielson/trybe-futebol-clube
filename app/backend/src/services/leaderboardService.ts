2c4c03c518c5f5da32f8c7a64491c977c460ec06
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-03-24 19:08:20 -0300

    Impl: getLeaderboardAway na camada services

--- app/backend/src/services/leaderboardService.ts
+++ app/backend/src/services/leaderboardService.ts
@@ -1,8 +1,9 @@
-import { ILeaderboards, ILeaderboardsHome } from '../interfaces/ILeaderboards';
+import { ILeaderboards, ILeaderboardsAway, ILeaderboardsHome } from '../interfaces/ILeaderboards';
 import Clubs from '../database/models/Clubs';
 import Matchs from '../database/models/Matchs';
 import { resultRanking, leaderboards } from '../functions/helpers';
 import { leaderBoardsHome } from '../functions/helpersLeaderBoardsHome';
+import { leaderBoardsAway } from '../functions/helpersLeaderBoardsAway';
 
 export default class LeaderBoardService {
   static async getLeaderboard() {
@@ -29,6 +30,21 @@ export default class LeaderBoardService {
         ],
       });
       const result = leaderBoardsHome(matchs as unknown as ILeaderboardsHome[]);
+      const ranking = resultRanking(result);
+      return ranking;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  static async getLeaderboardAway() {
+    try {
+      const matchs = await Clubs.findAll({
+        include: [
+          { model: Matchs, as: 'awayClub', where: { inProgress: false } },
+        ],
+      });
+      const result = leaderBoardsAway(matchs as unknown as ILeaderboardsAway[]);
       const ranking = resultRanking(result);
       return ranking;
     } catch (error) {
