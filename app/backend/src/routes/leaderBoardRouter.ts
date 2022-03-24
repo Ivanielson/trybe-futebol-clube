b64d965314817ae2873b851d41d83c1d5a191e1c
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-03-24 19:09:19 -0300

    Impl: Rota 'GET /leaderboard/away'

--- app/backend/src/routes/leaderBoardRouter.ts
+++ app/backend/src/routes/leaderBoardRouter.ts
@@ -5,5 +5,6 @@ const leaderboardRouter = Router();
 
 leaderboardRouter.get('/', LeaderBoardController.getLeaderboard);
 leaderboardRouter.get('/home', LeaderBoardController.getLeaderboardHome);
+leaderboardRouter.get('/Away', LeaderBoardController.getLeaderboardAway);
 
 export default leaderboardRouter;
