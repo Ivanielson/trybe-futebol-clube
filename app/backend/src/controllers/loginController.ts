a64ac4b00223146baa3643738820ef722e8ccbd4
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-03-25 09:32:47 -0300

    Impl: tipagem no retorno dos metodos da classe LoginController

--- app/backend/src/controllers/loginController.ts
+++ app/backend/src/controllers/loginController.ts
@@ -4,19 +4,19 @@ import LoginService from '../services/loginService';
 import StatusCode from '../enums/StatusCode';
 
 export default class LoginController {
-  static async authentication(req: Request, res: Response) {
+  static async authentication(req: Request, res: Response): Promise<Response | undefined> {
     try {
       const user = req.body as ILogin;
       const userValid = await LoginService.authentication(user.email);
 
-      res.status(StatusCode.OK).json(userValid);
+      return res.status(StatusCode.OK).json(userValid);
     } catch (error) {
       console.error(error);
     }
   }
 
-  static validateToken(req: Request, res: Response) {
-    const token = req.headers.authorization;
+  static validateToken(req: Request, res: Response): Response | undefined {
+    const token: string | undefined = req.headers.authorization;
     if (token) {
       const role = LoginService.validateToken(token);
       return res.status(StatusCode.OK).json(role);
